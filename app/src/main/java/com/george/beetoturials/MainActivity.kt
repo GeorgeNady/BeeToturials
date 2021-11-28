63c14b86abc5d1546ffe86a3af319529de8ab257
George <gorgonady.gn@gmail.com>
2021-11-28 12:24:43 +0200

    manual binding views

--- app/src/main/java/com/george/beetoturials/MainActivity.kt
+++ app/src/main/java/com/george/beetoturials/MainActivity.kt
@@ -2,10 +2,30 @@ package com.george.beetoturials
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var etEmail: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var btnLogin: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        etEmail = findViewById(R.id.etEmail)
+        etPassword = findViewById(R.id.etPassword)
+        btnLogin = findViewById(R.id.btnLogin)
+
+
+        btnLogin.setOnClickListener {
+
+            Log.d("MAinActivity", "onCreate: email: ${etEmail.text} pass: ${etPassword.text}")
+
+        }
+
     }
 }
